bd6e9d3b47910ce2f5f9dc7ad3991f6ecdbd3787
Ogan Özkul <oganozkul@gmail.com>
2023-05-05 23:08:01 +0200

    Add files via upload

---
+++ randStr.ps1
@@ -0,0 +1,27 @@
+# +---------------------------------------+
+# | MADE BY OGAN OEZKUL (AKA RYUGUU CHAN) |
+# +---------------------------------------+
+
+# Yea it's pretty rudimentary stuff...
+
+param
+(
+    [Parameter(Mandatory=$true)]
+    [int]$length
+)
+
+Write-Host $length
+
+[string]$passChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcedfghijklmnopqrstuvwxyz0123456789-*/+.-_:,;<>\\¦@#ç%&¬/|(¢)=?^~"
+[string]$output = ""
+
+[int]$maxRand = $passChars.Length
+
+for ($i = 0; $i -lt $length; $i++)
+{
+    $temp = Get-Random -Minimum 0 -Maximum $maxRand
+    $output += $passChars[$temp]
+}
+
+# outputting the result
+Write-Host $output
